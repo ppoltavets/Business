187a053459e39897478029c498852f48da5f8618
petrpoltavec <poltavets123123@gmail.com>
2023-11-04 15:02:49 +0300

    init basic classes

--- src/main/java/org/example/CalculateExitParams.java
+++ src/main/java/org/example/CalculateExitParams.java
@@ -4,11 +4,7 @@ public class CalculateExitParams {
 
     public double availableMoney(double salary, double rent, double food, double transport, double other) {
         CalculateEntryParams cep = new CalculateEntryParams();
-        cep.setSalary(salary);
-        cep.setRent(rent);
-        cep.setFood(food);
-        cep.setTransport(transport);
-        cep.setOther(other);
+        cep.CalculateEntryParamsEntity(200,200,200,200,200);
         Income income = new Income();
         income.IncomeCalc(200);
         return cep.getSalary() - cep.getRent() - cep.getFood() - cep.getTransport() - cep.getOther() + income.getIncome();
